83bb30893d239f0de6b796ee77c7881b3b9fb873
YuriAmani <yuriamani7@gmail.com>
2024-04-08 09:08:55 +0300

    dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainId = document.getElementById('main')
+mainId.remove()
+
+const newHeader = document.createElement('h1')
+//document.body.append(newHeader)
+
+newHeader.id = "victory";
+newHeader.textContent = "Yuri is the champion";
